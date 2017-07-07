5af8222dc37606f43db581e0ce0c980de3dfdce5
mozhavro <mozhario@gmail.com>
2017-07-08 01:03:15 +0300

    Added auth

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,10 @@
 class CardsController < ApplicationController
+    before_action :require_authentication, except: :index
+
+    def require_authentication
+        authenticate_or_request_with_http_basic do |u,p|
+            user = User.find_by(username: u)
+            true if user && user.authenticate(p)
+        end
+    end
 end
