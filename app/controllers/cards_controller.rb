2db63635e21e499b137af92caacfe172e96b9c0a
mozhavro <mozhario@gmail.com>
2017-07-09 13:28:28 +0300

    implemented cards addition backend

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,4 +6,29 @@ class CardsController < ApplicationController
             true if u==ENV['ADMIN_USERNAME'] && p==ENV['ADMIN_PASS']
         end
     end
+
+    def index
+        # params.each do |key,value|
+        #     Rails.logger.warn "Param #{key}: #{value}"
+        # end
+        
+        white_cards = params["white-cards"]
+        black_cards = params["black-cards"]
+
+        if white_cards
+            white_cards.each do |text|
+                unless text.strip.empty? 
+                    card = Card.create(text: text, card_type: "white")
+                end
+            end
+        end
+
+        if white_cards
+            black_cards.each do |text|
+                unless text.strip.empty? 
+                    card = Card.create(text: text, card_type: "black")
+                end
+            end
+        end
+    end
 end
